c76a656a5e06b24c14d9a27c9f565148b2fccd38
niculistana <niculistana@gmail.com>
2020-02-01 02:49:11 -0800

    Add DoozyUI and basic UI logic
    - Basic menus: Main Menu, Choice Menu, and Dialogue Menu
    - DoozyUI is a UI Framework:
    - DotTween is a dependency of DoozyUI

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine.UI;
+
+public class Game : MonoBehaviour
+{
+	private bool isGamePaused;
+
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown("Cancel")) {
+			isGamePaused = !isGamePaused;
+		}
+
+		if (isGamePaused) {
+			PauseGame();
+		} else {
+			ResumeGame();
+		}
+	}
+
+	void PauseGame()
+	{
+		UIView.ShowView("General", "Main Menu");
+		UIView.HideView("Game", "Game Menu");
+		UIView.HideView("Game", "Dialogue Menu");
+	}
+
+	void ResumeGame()
+	{
+		UIView.HideView("General", "Main Menu");
+		UIView.ShowView("Game", "Game Menu");
+		UIView.ShowView("Game", "Dialogue Menu");
+	}
+}
